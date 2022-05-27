21869f33d2a97ba5c3b51ad47d3fd73c1e2ffc8d
Thomas Bianchi <thomas.bianchi@mia-platform.eu>
2022-05-27 16:54:16 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# graceful-k8s-node-drain
-gracefully drain kubernetes node
+# Gracefully drain Kubernetes Node
+
+Draining a node with kubectl without a disruption budget [will make disservices](https://kubernetes.io/docs/tasks/administer-cluster/safely-drain-node/) because will delete all pods on the specified node assuming is possible given that that deployment is lacking the pod distruption budget.
+
+In the real world is possible to find a very important deployment without that setting.    
+
+This script will cordon the node, find all pods running on the node, identify the owner ( Deployment ) and if that deployment has ready replicas > 1 simply delete pod otherwise will issue `kubectl rollout restart <deployment>`.  
+
+This is a simple-minded approach, you should really configure a pod disruption budget. 
+
+## Usage
+
+```
+./graceful-drain.sh --node NODENAME
+```
+
---
+++ graceful-drain.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]
+  then
+    echo "error: usage ./graceful-drain.sh --node NODENAME" >&2
+    exit 3
+fi
+
+echo "condoning node $2"
+# echo debug kubectl cordon node $2
+kubectl get pods --all-namespaces --field-selector="spec.nodeName=$2" -o jsonpath='{range .items[*]}{.metadata.namespace},{.metadata.name},{.metadata.ownerReferences[0].kind},{.metadata.ownerReferences[0].name}{"\n"}{end}' | \
+while IFS="," read namespace name ownerKind ownerName ; do
+  if [ "$ownerKind" != "ReplicaSet" ] ; then
+    # ignore daemonsets or other owners
+    continue
+  fi
+  IFS="," read readyReplicas replicaOwner replicaOwnerName <<< $(kubectl get -n $namespace -o jsonpath='{.status.readyReplicas},{.metadata.ownerReferences[0].kind},{.metadata.ownerReferences[0].name}' ReplicaSet $ownerName)
+  if [ "$replicaOwner" != "Deployment" ] ; then
+    echo "error: $replicaOwner not expected" >&2
+    exit 2
+  fi
+  if [ $readyReplicas -le 1 ] ; then
+    echo "rolling restarting $replicaOwner $replicaOwnerName with $readyReplicas ready replicas"
+    # echo debug kubectl -n $namespace rollout restart $replicaOwner $replicaOwnerName
+  else
+    echo "deleting pod $name owned by $replicaOwner $replicaOwnerName with $readyReplicas ready replicas"
+    # echo debug kubectl -n $namespace delete pod $name
+    fi
+done
